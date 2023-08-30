e6f1cdf5bcbaded82f171096d31504feb23fd051
AryaneMachado <aryanecassimiro0309@gmail.com>
2023-08-30 07:17:58 -0300

    Atualização dia 30/08/23

--- Exercício em Sala/JavaSorts/src/javasorts/JavaSorts.java
+++ Exercício em Sala/JavaSorts/src/javasorts/JavaSorts.java
@@ -1,3 +1,14 @@
+<<<<<<< HEAD
+package javasorts;
+import java.util.Random;
+import java.util.Scanner;
+public class JavaSorts {
+    static int menuEntrada(){
+        Scanner scanner = new Scanner(System.in);
+        int op;
+        System.out.println("1-Gerar dados");
+        System.out.println("2-Entrar com dados");
+=======
 
 package javasorts;
 
@@ -15,10 +26,23 @@ public class JavaSorts {
         Scanner scanner = new Scanner(System.in);
         System.out.println("1 - Gerar Dados");
         System.out.println("2 - Entrar com Dados");
+>>>>>>> bd61cabfad2a9c358fe7daecf638c30b34e7c959
         op = scanner.nextInt();
         return op;
     }
     
+<<<<<<< HEAD
+        static int menuMetodos(){
+        Scanner scanner = new Scanner(System.in);
+        int op;
+        System.out.println("1-Bubble Sort");
+        System.out.println("2-Selection Sort");
+        System.out.println("3-Insertion Sort");
+        System.out.println("4-Compara métodos");
+        System.out.println("5-Busca Linear");
+        System.out.println("6-Busca Binária");
+        System.out.println("0-Sair");
+=======
     public static int menuSort() {
         int op;
         Scanner scanner = new Scanner(System.in);
@@ -27,19 +51,122 @@ public class JavaSorts {
         System.out.println("2 - SelectionSort");
         System.out.println("3 - InsertionSort");
         System.out.println("4 - Comparar algoritmos");
+>>>>>>> bd61cabfad2a9c358fe7daecf638c30b34e7c959
         op = scanner.nextInt();
         return op;
     }
     
+<<<<<<< HEAD
+=======
     public static void printArray(int array[]) {
         for(int i = 0; i < array.length; i++){
             System.out.print(array[i] + " | ");
         }
     }
+>>>>>>> bd61cabfad2a9c358fe7daecf638c30b34e7c959
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
+<<<<<<< HEAD
+        int tamanho,op,valorBusca;
+        long tempoInicial,tempoFinal;
+        
+        System.out.println("Tamanho do vetor:");
+        tamanho = scanner.nextInt();
+        int[] vetor = new int[tamanho];
+        op = menuEntrada();
+        switch(op){
+            case 1:for(int i=0;i<vetor.length;i++)
+                        vetor[i] = rand.nextInt(100);
+            break;   
+            case 2: System.out.println("Dados vetor:");
+                    for(int i=0;i<vetor.length;i++)
+                        vetor[i] = scanner.nextInt();
+            break;        
+       }// fim switch
+        System.out.println("Vetor original:");
+        printArray(vetor);
+        do{
+        op = menuMetodos();
+        switch(op){
+            case 3: InsertionSort.iSort(vetor);
+                    System.out.println("Ordenado:");
+                    printArray(vetor);
+            break;
+            case 5: System.out.println("Valor:");
+                    valorBusca=scanner.nextInt();
+                    if(Search.buscaLinear(vetor, valorBusca))
+                        System.out.println("Encontrado");
+                    else
+                        System.out.println("Inexistente");
+                    System.out.println("Comparacoes busca linear:"+ 
+                                         Search.compLinear);
+            break;
+            case 6: System.out.println("Valor:");
+                    valorBusca=scanner.nextInt();
+                    if(Search.buscaBinariaComentado(vetor, valorBusca))
+                        System.out.println("Encontrado");
+                    else
+                        System.out.println("Inexistente"); 
+                    System.out.println("Comparacoes busca binaria:"+ 
+                                         Search.compBinaria);
+            break;
+            case 0: System.out.println("Saindo");
+        }// fim switch
+        }while(op!=0);
+       /* int[] vetor2 = vetor.clone();
+        int[] vetor3 = vetor.clone();
+
+
+        //Bubble Sort
+        System.out.println("Bubble Sort");
+        tempoInicial = System.currentTimeMillis();
+        BubbleSort.bSort(vetor);
+        tempoFinal = System.currentTimeMillis();
+        System.out.println
+          ("Comparacoes:"+BubbleSort.compara);
+        System.out.println("Trocas:"+BubbleSort.trocas);
+        System.out.println("Tempo:"+(tempoFinal-tempoInicial));
+       //SelectionSort 
+       System.out.println("Selection Sort");
+       tempoInicial = System.currentTimeMillis();
+       SelectionSort.sSort(vetor2);
+        tempoFinal = System.currentTimeMillis();
+        System.out.println
+          ("Comparacoes:"+SelectionSort.compara);
+        System.out.println("Trocas:"+SelectionSort.trocas);
+        System.out.println("Tempo:"+(tempoFinal-tempoInicial));
+       
+       System.out.println("Insertion Sort");
+       tempoInicial = System.currentTimeMillis();
+        InsertionSort.iSort(vetor);
+        tempoFinal = System.currentTimeMillis();
+        System.out.println
+          ("Comparacoes:"+InsertionSort.compara);
+        System.out.println("Deslocamento:"+InsertionSort.deslocamento);
+        System.out.println("Tempo:"+(tempoFinal-tempoInicial));
+       
+        
+        System.out.println("Vetor ordenado");
+        //printArray(vetor);*/
+    }
+    
+    public static void printArray(int array[]){
+          for(int i=0;i<array.length;i++)
+              System.out.print(array[i]+"| ");
+          
+          System.out.println("\n");
+    }// fim printArray
+    
+    public static void printIntervalo(int array[], int ini, int fim){
+          for(int i=ini;i<=fim;i++)
+              System.out.print(array[i]+"| ");
+          
+          System.out.println("\n");
+    }// fim printArray
+    
+=======
         int tam;
         double tempoInicial, tempoFinal;
         
@@ -126,4 +253,5 @@ public class JavaSorts {
         
     }
     
+>>>>>>> bd61cabfad2a9c358fe7daecf638c30b34e7c959
 }
--- Exercício em Sala/JavaSorts/src/javasorts/SelectionSort.java
+++ Exercício em Sala/JavaSorts/src/javasorts/SelectionSort.java
@@ -1,9 +1,54 @@
+<<<<<<< HEAD
+=======
 
+>>>>>>> bd61cabfad2a9c358fe7daecf638c30b34e7c959
 package javasorts;
 
 import java.util.Scanner;
 
 public class SelectionSort {
+<<<<<<< HEAD
+    public static long compara=0,trocas=0;
+
+    private static int indexMinArray(int array[], int posAtual){
+       int k = posAtual; // k é o indice do menor
+       for(int i=posAtual+1;i<array.length;i++){
+           compara++;
+           if(array[i]<array[k])
+               k = i;
+       }// fim for    
+       return k; // indice do menor valor
+    }// fim indexMinArray
+//----------------------------------     
+    public static void sSort(int array[]){
+         for(int i=0;i<array.length-1;i++){
+             int k = indexMinArray(array, i);
+             // trocar valores de i com k
+             trocas++;
+             int temp = array[i];
+             array[i] = array[k];
+             array[k] = temp;
+         }
+    }
+//--------------------------------------------    
+        public static void sSortComentado(int array[]){
+         Scanner scanner = new Scanner(System.in);
+         for(int i=0;i<array.length-1;i++){
+             System.out.println("Fase "+ i);
+             int k = indexMinArray(array, i);
+             System.out.println
+               ("Atual:"+array[i]
+                +" Trocando com menor valor: "+array[k]);
+             scanner.nextLine();//pausa
+             // trocar valores de i com k
+             int temp = array[i];
+             array[i] = array[k];
+             array[k] = temp;
+             //------------------ mostrando o vetor
+             JavaSorts.printArray(array);
+             scanner.nextLine();//pausa
+         }
+=======
     
     public static long compara = 0, troca = 0;
     
@@ -46,5 +91,6 @@ public class SelectionSort {
             JavaSorts.printArray(array);
             scanner.nextLine();
         }
+>>>>>>> bd61cabfad2a9c358fe7daecf638c30b34e7c959
     }
 }
